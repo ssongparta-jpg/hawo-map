edc2cfbe6a4decb4f2d0fc01f572a41b499b8e1f
Rhythmfactory <64122960+junhee6296@users.noreply.github.com>
2026-02-09 10:35:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -685,7 +685,6 @@ bindEvents() {
                 <ul class="popup-info-list">
                     <li><span class="label">학교 수</span> <span class="value"><strong>${targets.length - 1}</strong>개교</span></li>
                     <li><span class="label">총 학생 수</span> <span class="value"><strong>${stats.s.toLocaleString()}</strong>명</span></li>
-                    <li><span class="label">총 학급 수</span> <span class="value"><strong>${stats.c.toLocaleString()}</strong>개</span></li>
                     <li><span class="label">총 교사 수</span> <span class="value"><strong>${stats.t.toLocaleString()}</strong>명</span></li>
                 </ul>
             </div>
